8e83a2d7b86379e0ea84713905b880708a2b2092
Lasse Letager Hansen <lasse@letager.dk>
2021-12-21 18:22:46 +0100

    Improved exists and forall

--- language/src/rustspec_to_coq.rs
+++ language/src/rustspec_to_coq.rs
@@ -1394,7 +1394,7 @@ fn translate_quantified_expression<'a>(
         Quantified::Forall(ids, qe2) => RcDoc::as_string("forall")
             .append(RcDoc::space())
             .append(RcDoc::intersperse(
-                ids.iter().map(|(x, _)| translate_ident(x.clone())),
+                ids.into_iter().map(|(x, (typ , _))| translate_ident(x.clone()).append(RcDoc::as_string(" : ")).append(translate_base_typ(typ))),
                 RcDoc::space(),
             ))
             .append(RcDoc::as_string(","))
@@ -1403,9 +1403,11 @@ fn translate_quantified_expression<'a>(
         Quantified::Exists(ids, qe2) => RcDoc::as_string("exists")
             .append(RcDoc::space())
             .append(RcDoc::intersperse(
-                ids.iter().map(|(x, _)| translate_ident(x.clone())),
+                ids.into_iter().map(|(x, (typ, _))| translate_ident(x.clone()).append(RcDoc::as_string(" : ")).append(translate_base_typ(typ))),
                 RcDoc::space(),
             ))
+            .append(RcDoc::as_string(","))
+            .append(RcDoc::line())
             .append(translate_quantified_expression(*qe2, top_ctx)),
         Quantified::Implication(qe2, qe3) => translate_quantified_expression(*qe2, top_ctx)
             .append(RcDoc::space())
