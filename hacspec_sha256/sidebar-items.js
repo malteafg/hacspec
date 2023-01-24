69f853f7209adc326f44b38a33d59308f9dcddd4
malteafg <malteafg@users.noreply.github.com>
2023-01-24 12:43:47 +0000

    deploy: 18744ac5c113a8450183c2ef83913c81ecb06529

--- hacspec_sha256/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"constant":[["HASH_SIZE",""],["K_SIZE",""]],"fn":[["hash",""],["sha256",""]],"struct":[["Block","Fixed length byte array."],["Hash","Fixed length byte array."],["OpTableType","Fixed length byte array."],["RoundConstantsTable","Fixed length byte array."],["Sha256Digest","Fixed length byte array."]]});
